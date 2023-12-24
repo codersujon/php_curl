56786d357e20d05984e649f9bfaeae0598f39f4e
MD SUJAN AHMED <csesujon155@gmail.com>
2023-12-24 21:28:44 +0600

    get_userlist response to array

--- get_userlist.php
+++ get_userlist.php
@@ -1,4 +1,38 @@
 <?php 
+
+    $ch = curl_init();
+
+    $url = "https://reqres.in/api/users?page=2";
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     
+    $resp = curl_exec($ch);
+
+    if($error = curl_error($ch)){
+        echo $error;
+    }else{
+        # Decode JSON Data Response
+        $data = json_decode($resp, true);
+
+        echo "<pre>";
+        print_r($data);
+    }
+
+
+    curl_close($ch);
+
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
